1f5a63f96cb9ed7e16b947e9aafe2eb2fa6ea535
amidos2006 <amidos2002@hotmail.com>
2022-07-08 17:39:43 +0200

    adding some progress bars

--- run_inference.py
+++ run_inference.py
@@ -6,8 +6,10 @@ import games.binary as binary
 from games.helper import get_number_regions
 
 from nn.model import SOFTMAX_ACT, GREEDY_ACT, SMNN
-import numpy as np
 from nn.helper import transform_input
+
+import numpy as np
+from tqdm import tqdm, trange
 
 import os
 import shutil
@@ -25,100 +27,106 @@ if __name__ == "__main__":
     fitness = binary.fitness
     behaviors = binary.behaviors
 
-    max_iterations = 10
+    max_iterations = width * height
     input_size = 8
     conditional = False
     action_type = SOFTMAX_ACT                         # type of mutation when using the network
     random_order = False
 
+    visualize = True
+    number_times = 1
     model_path = "results/es/0/binary_evol_2_normal/1999"
     animation_path = "results/animations"
 
+    for i in range(number_times):
+        print(f"Generating {i} Level: ")
+        target = []
+        for i in range(num_behaviors):
+            target.append(np.random.randint(behavior_bins) / (1.0 * behavior_bins))
+        channels = num_tiles
+        if num_tiles <= 2:
+            channels = 1
 
-    target = []
-    for i in range(num_behaviors):
-        target.append(np.random.randint(behavior_bins) / (1.0 * behavior_bins))
-    channels = num_tiles
-    if num_tiles <= 2:
-        channels = 1
+        model = torch.load(os.path.join(model_path, "model"))
+        start = init(width, height)
 
-    model = torch.load(os.path.join(model_path, "model"))
-    start = init(width, height)
+        print(f"\tStart Fitness: {fitness(start, [])}")
+        print(f"\tStart Fitness: {behaviors(start, [], behavior_bins)}")
 
-    print(f"Start Fitness: {fitness(start, [])}")
-    print(f"Start Fitness: {behaviors(start, [], behavior_bins)}")
+        tiles = []
+        for y in range(height):
+            for x in range(width):
+                tiles.append({"x": x, "y": y})
 
-    tiles = []
-    for y in range(height):
-        for x in range(width):
-            tiles.append({"x": x, "y": y})
-
-    frames = []
-    done = False
-    level = start.copy()
-    with torch.no_grad():
-        for i in range(max_iterations):
-            if random_order:
-                np.random.shuffle(tiles)
-            change = False
-            all_obs = []
-            for t in tiles:
-                x,y = t["x"], t["y"]
-                obs = transform_input(level, {"x":x,"y":y}, input_size, channels)
-                all_obs.append(obs)
-            all_obs = np.array(all_obs)
-            if conditional:
-                values = model(torch.tensor(all_obs.reshape(-1,channels,input_size,input_size)).float(),\
-                                   torch.tensor(np.array(target).reshape(1,-1).repeat(len(all_obs), axis=0))).float())
-            else:
-                values = model(torch.tensor(all_obs.reshape(-1,channels,input_size,input_size)).float(), None)
-            values = F.softmax(values, dim=1).numpy()
-            for ai,t in enumerate(tiles):
-                x,y = t["x"], t["y"]
-                if action_type == SOFTMAX_ACT:
-                    action = np.random.choice(list(range(num_tiles + 1)), p=values[ai].flatten())
+        frames = []
+        done = False
+        level = start.copy()
+        with torch.no_grad():
+            for i in trange(max_iterations, leave=False):
+                if random_order:
+                    np.random.shuffle(tiles)
+                change = False
+                all_obs = []
+                for t in tiles:
+                    x,y = t["x"], t["y"]
+                    obs = transform_input(level, {"x":x,"y":y}, input_size, channels)
+                    all_obs.append(obs)
+                all_obs = np.array(all_obs)
+                if conditional:
+                    values = model(torch.tensor(all_obs.reshape(-1,channels,input_size,input_size)).float(),\
+                                       torch.tensor(np.array(target).reshape(1,-1).repeat(len(all_obs), axis=0)).float())
                 else:
-                    action = values[ai].argmax().item()
-                if action > 0:
-                    level[y][x] = action - 1
-                    change = True
-                frames.append(binary.render(level.copy()))
-            if get_number_regions(level, [1]) == 1:
-                print(f"One Region: {i}")
-                done = True
-                break
-            if done:
-                break
-            if not change and action_type != SOFTMAX_ACT:
-                print(f"Stablized: {i}")
-                break
+                    values = model(torch.tensor(all_obs.reshape(-1,channels,input_size,input_size)).float(), None)
+                values = F.softmax(values, dim=1).numpy()
+                for ai,t in enumerate(tiles):
+                    x,y = t["x"], t["y"]
+                    if action_type == SOFTMAX_ACT:
+                        action = np.random.choice(list(range(num_tiles + 1)), p=values[ai].flatten())
+                    else:
+                        action = values[ai].argmax().item()
+                    if action > 0:
+                        level[y][x] = action - 1
+                        change = True
+                    frames.append(binary.render(level.copy()))
+                if get_number_regions(level, [1]) == 1:
+                    print(f"\n\tOne Region: {i}")
+                    done = True
+                    break
+                if done:
+                    break
+                if not change and action_type != SOFTMAX_ACT:
+                    print(f"\tStablized: {i}")
+                    break
 
-    print(f"Final Fitness: {fitness(level, [])}")
-    print(f"Final Fitness: {behaviors(level, [], behavior_bins)}")
+        print(f"\tFinal Fitness: {fitness(level, [])}")
+        print(f"\tFinal Fitness: {behaviors(level, [], behavior_bins)}")
 
-    for i in range(20):
-        frames.append(frames[-1].copy())
+        if visualize:
+            print("\tAnimating...")
+            for i in range(20):
+                frames.append(frames[-1].copy())
 
-    def init_display():
-        im.set_data(start)
-        return [im]
+            def init_display():
+                im.set_data(start)
+                return [im]
 
-    def animate_display(i):
-        im.set_data(frames[i])
-        return [im]
+            def animate_display(i):
+                im.set_data(frames[i])
+                return [im]
 
-    fig = plt.figure()
-    plt.axis('off')
-    im = plt.imshow(start)
-    plt.tight_layout()
+            fig = plt.figure()
+            plt.axis('off')
+            im = plt.imshow(start)
+            plt.tight_layout()
 
-    anim = animation.FuncAnimation(fig, animate_display, init_func=init_display,\
-                                   frames=len(frames), interval=15, blit=True)
-    plt.close(anim._fig)
+            anim = animation.FuncAnimation(fig, animate_display, init_func=init_display,\
+                                           frames=len(frames), interval=15, blit=True)
+            plt.close(anim._fig)
 
-    # Call function to display the animation
-    if not os.path.exists(animation_path):
-        os.makedirs(animation_path)
-    prev_anims = [f for f in os.listdir(animation_path) if "animation" in f]
-    anim.save(os.path.join(animation_path, f"animation_{len(prev_anims)}.mp4"))
-    # save_gif("test.gif", np.array(frames), 60, 50)
+            # Call function to display the animation
+            if not os.path.exists(animation_path):
+                os.makedirs(animation_path)
+            prev_anims = [f for f in os.listdir(animation_path) if "animation" in f]
+            final_path = os.path.join(animation_path, f"animation_{len(prev_anims)}.mp4")
+            anim.save(final_path)
+            print(f"\tAnimation is in: {final_path}")
